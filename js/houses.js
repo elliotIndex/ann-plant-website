b8ccec5ccece55bb1f56bd64e9b59287733fbce3
Elliot Plant <elliotaplant@gmail.com>
2016-11-21 20:37:57 -0800

    Mose setuf

--- js/houses.js
+++ js/houses.js
@@ -4,7 +4,7 @@ var houses = [{
     pictureFolder: 'thousandOaks'
   }, {
     name: '200 Small St.',
-    description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using "Content here, content here", making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for "lorem ipsum" will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).',
+    description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using "Content here, content here", making it look like readable English.',
     pictureFolder: 'smallSt'
   }
 ];
@@ -13,14 +13,23 @@ $(document).ready(function() {
   var housesList = $('.houses-list');
 
   houses.forEach(function(house){
-    var houseLi = $("<li>", { "class": "house-row" });
+    var houseLi = $("<li>", { "class": "house-row row" });
     // var houseDiv = $("<div>", { "class": "house" });
-    var smallPic = $("<img>", { "src": "img/houses/" + house.pictureFolder + "/small.png"});
-    var title = $("<span>" + house.name + "</span>", { "class": "house-title" });
-    houseLi.click(function(){ console.log("clickin on fo fos"); });
+    var picDiv = $("<div>", { "class": "col-lg-5 col-sm-6" });
+    var smallPic = $("<img>", {
+      "src": "img/houses/" + house.pictureFolder + "/large.png",
+      "class": "house-pic"
+    });
+    picDiv.append(smallPic);
 
-    houseLi.append(smallPic);
-    houseLi.append(title);
+    var textDiv = $("<div>", { "class": "col-lg-6 col-lg-offset-1 col-sm-6" });
+    var title = $("<h3>" + house.name + "</h3>", { "class": "house-title" });
+    var description = $("<div>" + house.description + "</div>", { "class": "house-description" });
+    textDiv.append(title);
+    textDiv.append(description);
+
+    houseLi.append(picDiv);
+    houseLi.append(textDiv);
     $(housesList).append(houseLi);
   });
 });
